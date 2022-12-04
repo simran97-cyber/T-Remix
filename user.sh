0e35829aad8286ffcfe15c8f33524f470a25c307
simran97-cyber <64253817+simran97-cyber@users.noreply.github.com>
2022-12-05 00:14:26 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -258,7 +258,7 @@ tput cnorm
 echo "PS1='\[\e[1;34m
 
 
-\a├─[\033[1;32m\w\033[1;34m]\033[1;34m
+\a─[\033[1;32m\w\033[1;34m]\033[1;34m
 \[\e[34m\]└─>\[\e[35m\]$varterm\[\e[34m\]\[\e[1;32m\] ' " > ash.txt 
 
 cat "t-ban.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
