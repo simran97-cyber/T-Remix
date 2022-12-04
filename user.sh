2b687a8b4ec4a9116b9ab584d06ecd05c95b950c
simran97-cyber <64253817+simran97-cyber@users.noreply.github.com>
2022-12-05 00:03:29 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -256,8 +256,8 @@ tput cnorm
 # custom PS1, validating user input and user define variable
 
 echo "PS1='\[\e[1;34m
-\a┌──\a─T─I─M─E─\a──┐\033[1;34m\a┌──\a─D─A─T─E─\a───>\033[1;34m
-\a┌─[\033[1;93m \@\033[1;34m ]──[\033[1;93m \d\033[1;34m ]\033[1;34m
+
+
 \a├─[\033[1;32m\w\033[1;34m]\033[1;34m
 \[\e[34m\]└─>\[\e[35m\]$varterm\[\e[34m\]\[\e[1;32m\] ' " > ash.txt 
 
