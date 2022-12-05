5737de7f946f6fbb941c640918ec343e9dde696e
simran97-cyber <64253817+simran97-cyber@users.noreply.github.com>
2022-12-05 12:06:13 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -254,8 +254,8 @@ fi
 done
 tput cnorm
 # custom PS1, validating user input and user define variable
-echo "PS1='\[\e[1;34m  
-\a\033[1;93m\033[1;34m cat "Simr.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+echo "PS1='\[\e[1;34m] 
+cat "Simr.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
 \a─[\033[1;32m\w\033[1;34m]\033[1;34m
 \[\e[34m\]└─>\[\e[35m\]$varterm\[\e[34m\]\[\e[1;32m\] ' " > ash.txt 
 
